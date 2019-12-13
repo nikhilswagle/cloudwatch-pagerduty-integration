9ec64ab43af4f33156d82eaa359247eee6701bfa
Damodar Shenvi Wagle <damodaw@amazon.com>
2019-12-13 01:17:15 -0500

    updated code to match Pager Duty alert message format

--- lambda/event_handler.py
+++ lambda/event_handler.py
@@ -14,23 +14,23 @@ def handle_event(event, context):
     logger.info('EVENT:' + json.dumps(event, default=str))
     response = {}
     try:
-        # Get formatted message
-        message = message_formatter.format(event)
-        jsonMessage = json.dumps(message)
-        logger.info(jsonMessage)
+        # Get formatted pagerduty message
+        pagerdutyMessage = message_formatter.format(event)
+        jsonPagerdutyMessage = json.JSONEncoder().encode(pagerdutyMessage)
+        logger.info(jsonPagerdutyMessage)
 
-        # Send message to SNS
+        # Send pagerdutyMessage to SNS
         client = boto3.client('sns')
         snsResp = client.publish(
             TopicArn=os.environ['SNS_TOPIC_ARN'],
-            Message=jsonMessage,
-            Subject=message['Title']
+            Message=jsonPagerdutyMessage,
+            Subject=pagerdutyMessage['NewStateValue'] + ': "' + pagerdutyMessage['AlarmName'] + '" in ' + pagerdutyMessage['Region']
         )
         response['status'] = 'SUCCESS'
-        response['message'] = snsResp
+        response['pagerdutyMessage'] = snsResp
     except Exception as e:
         logger.error(e)
         response['status'] = 'FAILED'
-        response['message'] = 'Failed to publish message to SNS Topic'
+        response['pagerdutyMessage'] = 'Failed to publish pagerdutyMessage to SNS Topic'
     logger.info(response)
     return response
--- lambda/message_formatter.py
+++ lambda/message_formatter.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -9,33 +10,42 @@ Format message to be sent to pager duty via SNS.
 '''
 def format(event):
     message = json.load(open('message.json', 'r'))
-    message['Severity'] = __getSeverity(event)
-    message['Client'] = 'Amazon Cloudwatch Alarm'
-    message['Alarms'] = event['resources']
-    message['Title'] = event['detail']['alarmName']
-    message['Description'] = event['detail']['configuration']['description'] + '/n' + event['detail']['state']['reason']
-    message['Details'] = __getMetricDimensions(event)
+    message['AlarmName'] = event['detail']['alarmName']
+    if 'description' in event['detail']['configuration']:
+        message['AlarmDescription'] = event['detail']['configuration']['description']
+    else:
+        message['AlarmDescription'] = event['detail']['alarmName']
+    message['AWSAccountId'] = event['account']
+    message['NewStateValue'] = event['detail']['state']['value']
+    message['NewStateReason'] = event['detail']['state']['reason']
+    message['StateChangeTime'] = event['detail']['state']['timestamp']
+    message['Region'] = event['region']
+    message['OldStateValue'] = event['detail']['previousState']['value']
+    message = __updateMessageWithAlarmDetails(event, message)
     return message
 
 '''
-Collects metric dimensions for given alarm
+Collects required alarm details and updates them into the message to be sent out to pager duty.
 '''
-def __getMetricDimensions(event):
-    dimensions = {}
-    for metric in event['detail']['configuration']['metrics']:
-        if 'metricStat' in metric and 'dimensions' in metric['metricStat']['metric']:
-            for key, value in metric['metricStat']['metric']['dimensions'].items():
-                if key not in dimensions:
-                    dimensions[key] = []
-                dimensions[key].append(value)
-    return dimensions
-
-'''
-Figures out severity of the message.
-'''
-def __getSeverity(event):
-    '''
-    Implement method if required to figure out severity.
-    Default is CRITICAL
-    '''
-    return 'CRITICAL'
+def __updateMessageWithAlarmDetails(event, message):
+    client = boto3.client('cloudwatch')
+    response = client.describe_alarms(
+        AlarmNames=[
+            event['detail']['alarmName']
+        ]
+    )
+    # Get first item from the response since we are expecting only one alarm
+    metricAlarm = response['MetricAlarms'][0]
+    message['Trigger']['MetricName'] = metricAlarm['MetricName']
+    message['Trigger']['Namespace'] = metricAlarm['Namespace']
+    message['Trigger']['Period'] = metricAlarm['Period']
+    message['Trigger']['Statistic'] = metricAlarm['Statistic']
+    message['Trigger']['ComparisonOperator'] = metricAlarm['ComparisonOperator']
+    message['Trigger']['EvaluationPeriods'] = metricAlarm['EvaluationPeriods']
+    message['Trigger']['Threshold'] = metricAlarm['Threshold']
+    if 'Unit' in metricAlarm:
+        message['Trigger']['Unit'] = metricAlarm['Unit']
+    if 'TreatMissingData' in metricAlarm:
+        message['Trigger']['TreatMissingData'] = metricAlarm['TreatMissingData']
+    message['Trigger']['Dimensions'] = metricAlarm['Dimensions']
+    return message
